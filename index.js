96bf5f8912757cc4fcc69e73dbf2bb4e3a863e6b
Fabian Stehle <fabi@fstehle.de>
2015-11-29 20:12:34 +0100

    Implement proxy

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 'use strict';
 var http = require('http');
+var https = require('https');
 
+var page = "gasthof.roessle"
 var port = process.env.PORT || 8080;
+var appId = process.env.FB_APP_ID
+var appSecret = process.env.FB_APP_SECRET
+
+var baseURL = "https://graph.facebook.com";
+var tokenURL = "#{baseURL}/oauth/access_token?client_id=#{appID}&client_secret=#{appSecret}&grant_type=client_credentials";
+
+var accessToken = appId + "|" + appSecret
+var path = "/v2.5/" + page + "/events?access_token=" + accessToken + "&format=json"
+var httpOptions = {
+  hostname: "graph.facebook.com",
+  port: 443,
+  path: path,
+  method: "GET",
+  headers: {
+    'Accept': "application/json"
+  }
+};
+
 
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+  https.get(httpOptions, function(fbRes) {
+    console.log(fbRes);
+    if (fbRes.statusCode === 200) {
+      response.writeHead(fbRes.statusCode, {
+        "Content-Type": fbRes.headers["content-type"],
+        "ETag": fbRes.headers["etag"],
+        "facebook-api-version": fbRes.headers["facebook-api-version"]
+      });
+      fbRes.pipe(response, {end:true});
+    } else {
+      response.writeHead(fbRes.statusCode, {"Content-Type": "application/json"});
+      response.end({ message: "error" });
+    }
+  });
 });
 
 server.listen(port);
 
-console.log("Server running at http://127.0.0.1:8000/");
+console.log("Server running at port " + port + "\n");
